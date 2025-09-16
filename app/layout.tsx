1b50c346667b257aa4663c35f65f685fb0bc34f5
nermeennasim <63897480+nermeennasim@users.noreply.github.com>
2025-09-15 18:54:06 -0700

    added g-tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -120,8 +120,35 @@ export default function MainLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				{/* <!-- Google Tag Manager --> */}
+				{/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
+				<script
+					id="gtm-script"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M8SHBZ4D');`,
+					}}
+				/>
+				{/* <!-- End Google Tag Manager --> */}
+				<link rel="icon" href="images/favicon.ico" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white-smoke text-gray-900`}>
+				{/* <!-- Google Tag Manager (noscript) --> */}
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-M8SHBZ4D"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}></iframe>
+				</noscript>
+				{/* <!-- End Google Tag Manager (noscript) --> */}
+
 				{children}
 			</body>
 		</html>
